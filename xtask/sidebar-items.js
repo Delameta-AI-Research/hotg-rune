d75d1a41f6e2aa5f7646f7f58606123cb42a4a42
Michael-F-Bryan <michaelfbryan@gmail.com>
2021-09-07 15:10:23 +0000

    Deploying to gh-pages from @ hotg-ai/rune@999c54f4232b65b444c1e29268bfeb12aa24a6cb 🚀

--- xtask/sidebar-items.js
+++ xtask/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Command",""]],"fn":[["is_pre_commit",""],["main",""],["project_root",""],["run_pre_commit_hook",""]],"mod":[["bulk_copy",""],["check_manifests",""],["dist",""]]});
+initSidebarItems({"enum":[["Command",""]],"fn":[["main",""],["project_root",""]],"mod":[["bulk_copy",""],["check_manifests",""],["dist",""]]});

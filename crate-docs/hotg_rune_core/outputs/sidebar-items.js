d39f1066b12c2029ffbed9f93490a606159e7632
Michael-F-Bryan <michaelfbryan@gmail.com>
2022-01-24 02:37:24 +0000

    Deploying to gh-pages from @ hotg-ai/rune@18b744e88f561e952c23dcd1902b0337c1b9f44e 🚀

--- crate-docs/hotg_rune_core/outputs/sidebar-items.js
+++ crate-docs/hotg_rune_core/outputs/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["BLE",""],["PIN",""],["SERIAL","A serial device which consumes JSON-encoded data."],["WIFI",""]],"fn":[["all",""],["from_name",""],["name",""]]});
+initSidebarItems({"constant":[["BLE",""],["PIN",""],["SERIAL","A serial device which consumes JSON-encoded data."],["TENSOR","A raw tensor output."],["WIFI",""]],"fn":[["all",""],["from_name",""],["name",""]]});
